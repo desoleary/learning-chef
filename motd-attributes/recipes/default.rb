9ef6547ce3b5ae13865e304147e59b356fd52728
Desmond O''Leary <desolear@cisco.com>
2017-06-11 19:06:40 -0600

    starting chef attribtues

--- motd-attributes/recipes/default.rb
+++ motd-attributes/recipes/default.rb
@@ -4,6 +4,10 @@
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
 node.default['motd-attributes']['message'] = "It's a wonderful day today!"
+node.default['motd-attributes']['company'] = 'My Company'
+node.default['motd-attributes']['message'] = "It's a wonderful day today!"
+
+include_recipe 'motd-attributes::message'
 
 template '/etc/motd' do
   source 'motd.erb'
