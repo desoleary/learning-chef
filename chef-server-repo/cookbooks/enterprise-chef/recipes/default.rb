a19d6b23f5c4589b13fc84d289aef90f7a4ead40
Desmond O''Leary <desolear@cisco.com>
2017-06-11 19:27:37 -0600

    starting chef attribtues

--- chef-server-repo/cookbooks/enterprise-chef/recipes/default.rb
+++ chef-server-repo/cookbooks/enterprise-chef/recipes/default.rb
@@ -6,10 +6,20 @@
 package_url = node['enterprise-chef']['url']
 package_name = ::File.basename(package_url)
 package_local_path = "#{Chef::Config[:file_cache_path]}/#{package_name}"
+
 # omnibus_package is remote (i.e., a URL) let's download it
 remote_file package_local_path do
   source package_url
 end
-package package_local_path
+
+package package_name do
+  source package_local_path
+  provider Chef::Provider::Package::Rpm
+  notifies :run, 'execute[chef-server-ctl reconfigure]', :immediately
+end
+
 # reconfigure the installation
-execute 'chef-server-ctl reconfigure'
+execute 'chef-server-ctl reconfigure' do
+  command 'chef-server-ctl reconfigure'
+  action :nothing
+end
